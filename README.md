f0a95dfc924ea0c97468ad0df9053569fcc43407
Davis <davis.benjamin41902@gmail.com>
2025-02-12 23:19:44 -0800

    kind of working????

---
+++ README.md
@@ -0,0 +1,6 @@
+## STEPS TO DO THIS SHIT
+
+1. Create a new worker: `wrangler init <NAME>`
+2. Create a new KV namespace: `npx wrangler kv namespace create <BINDING_NAME>`
+3. Add the KV namespace to your wrangler config. Will be outputted when you create the KV namespace.
+4. Add in your open auth code.
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,71 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { subjects } from "./subjects";
+import { CloudflareStorage } from "@openauthjs/openauth/storage/cloudflare";
+import { issuer } from "@openauthjs/openauth";
+import { GithubProvider } from "@openauthjs/openauth/provider/github";
+import { Octokit } from "@octokit/core";
+
+declare global {
+  interface Env {
+    davis_auth_server: KVNamespace;
+    GITHUB_CLIENT_ID: string;
+    GITHUB_CLIENT_SECRET: string;
+  }
+}
 
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
+  async fetch(request, env, ctx): Promise<Response> {
+    const getUser = async (accessToken: string) => {
+      const octokit = new Octokit({
+        auth: accessToken,
+      });
+
+      const userInfo = await octokit.request("GET /user", {
+        headers: {
+          "X-GitHub-Api-Version": "2022-11-28",
+        },
+      });
+
+      console.log(userInfo);
+
+      const userData = {
+        id: userInfo.data.id.toString(),
+        email: userInfo.data.email ?? "bad man",
+      };
+
+      const user = await env.davis_auth_server.get(userData.id);
+
+      if (!user) {
+        await env.davis_auth_server.put(userData.id, userData.email);
+      }
+
+      return userData;
+    };
+
+    return issuer({
+      subjects,
+      storage: CloudflareStorage({
+        namespace: env.davis_auth_server,
+      }),
+      providers: {
+        github: GithubProvider({
+          clientID: env.GITHUB_CLIENT_ID,
+          clientSecret: env.GITHUB_CLIENT_SECRET,
+          scopes: ["email"],
+        }),
+      },
+      success: async (ctx, value) => {
+        if (value.provider === "github") {
+          console.log("GITHUB ID :0", value.clientID);
+
+          const user = await getUser(value.tokenset.access);
+
+          return ctx.subject("user", {
+            id: user.id,
+            email: user.email,
+          });
+        }
+        throw new Error("Invalid provider");
+      },
+    }).fetch(request, env, ctx);
+  },
 } satisfies ExportedHandler<Env>;
